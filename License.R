fb07baaaecfec0cfb97b75e6f3f4cf3a658f4286
Simone <garausimone93@ikmail.com>
2024-03-20 15:13:05 +0100

    primo commit

---
+++ License.R
@@ -0,0 +1,20 @@
+# License issued by BayesFusion Licensing Server
+#
+# This code must be executed before any other rSMILE object is created 
+rSMILE::License(paste(
+	"SMILE LICENSE c797856d 96979857 143a9f81 ",
+	"THIS IS AN ACADEMIC LICENSE AND CAN BE USED ",
+	"SOLELY FOR ACADEMIC RESEARCH AND TEACHING, ",
+	"AS DEFINED IN THE BAYESFUSION ACADEMIC ",
+	"SOFTWARE LICENSING AGREEMENT. ",
+	"Serial #: 2tqmgpr6po0o7k0i6x5v5cc7u ",
+	"Issued for: Simone Garau (20005068@studenti.uniupo.it) ",
+	"Academic institution: Universit\u00e0 degli Studi del Piemonte Orientale ",
+	"Valid until: 2024-09-21 ",
+	"Issued by BayesFusion activation server",
+	sep=""),as.integer(c(
+	-118,-63,-54,-11,-128,58,-56,42,64,117,79,-115,12,127,93,94,
+	-121,-4,38,-104,11,-88,91,-114,-16,-4,36,79,-117,-22,-31,-81,
+	-37,55,109,-39,104,41,96,33,-85,-45,111,-7,-106,75,-116,-33,
+	-95,85,86,-98,-36,116,7,-26,-8,94,119,47,70,14,-30,62))
+)
---
+++ problema_1.py
@@ -0,0 +1,4 @@
+import pysmile
+import pysmile_license
+
+
